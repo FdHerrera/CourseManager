7087a2f93d860beda4f92f421d11874b81f3e1a5
Federico <soyfdherrera@gmail.com>
2021-08-10 21:58:48 -0300

    Created Auth controller and register endpoint

---
+++ src/main/java/com/institution/manager/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.institution.manager.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth")
+public class AuthController {
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<?> registerUser(@RequestBody newUserDto newUserDto){
+        return null;
+    }
+
+}
