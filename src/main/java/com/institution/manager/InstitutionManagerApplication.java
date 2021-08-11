47538690912b41e2d639bb5855554b7a3425c016
Federico <soyfdherrera@gmail.com>
2021-08-10 21:50:58 -0300

    Renamed

--- src/main/java/com/institution/manager/InstitutionManagerApplication.java
+++ src/main/java/com/institution/manager/InstitutionManagerApplication.java
@@ -1,4 +1,4 @@
-package com.institution.management;
+package com.institution.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/institution/manager/InstitutionManagerApplicationTests.java
+++ src/test/java/com/institution/manager/InstitutionManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.institution.management;
+package com.institution.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
