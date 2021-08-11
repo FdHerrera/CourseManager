1bea55f342d49a87cde1b461b8ff72d6244169fa
Federico <soyfdherrera@gmail.com>
2021-08-10 21:43:02 -0300

    Bootstrapped project with Spring Initializr

---
+++ src/test/java/com/institution/management/InstitutionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institution.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
